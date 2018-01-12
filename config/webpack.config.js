a13c6b626777fba4c6dd5b6c0bfedbfde9f17c81
Peter Johnson <pjohnson@cluricon.com>
2018-01-11 22:06:10 -0500

    adding font loader

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -14,6 +14,10 @@ module.exports = {
             {
                 test: /\.css$/,
                 loader: 'style-loader!css-loader'
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2)$/,
+                loader: 'file?name=public/fonts/[name].[ext]'
             }
         ]
     },
