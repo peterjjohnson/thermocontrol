a671d2daa4fc2a15f7f4042643caddf941af22cb
Peter Johnson <pjohnson@cluricon.com>
2018-01-11 19:29:30 -0500

    using absolute path

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static('public'))
 
 // Front page of the app
 app.get('/', (req, res) => {
-    res.sendFile('index.html')
+    res.sendFile(`${__dirname}/public/index.html`)
 })
 
 try {
